a0d36e9dc2884b10f8226d773226870ee0bf8872
Thafsi PV <pvthafsi@gmail.com>
2023-05-24 16:25:05 +0530

    details and readme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/header/Header";
+import { Outlet, createBrowserRouter } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import ProductDetails from "./pages/poductDetails/ProductDetails";
+import ProductList from "./pages/productList/ProductList";
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export const AppRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <ProductList />,
+      },
+      {
+        path: "/product/:id",
+        element: <ProductDetails />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
 const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const productList = require("./productList.json");
+
+app.get("/api/products", (req, res) => {
+  res.json(productList);
+});
+
+app.get("/api/product/:id", (req, res) => {
+  const { id } = req.params;
+  const data = productList.filter((item) => item.id == id);
+  res.json(data);
+});
+
+const PORT = 3006;
+app.listen(PORT, () => console.log("server running"));
